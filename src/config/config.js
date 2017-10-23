fc565dcc106576e3c75f7d0d4bbada93c4461b49
Ale Figueroa <yknx.4.b@gmail.com>
2017-10-23 11:50:41 -0500

    Anything should be required

--- src/config/config.js
+++ src/config/config.js
@@ -1,15 +1,2 @@
-import invariant from 'invariant'
 import dotenv from 'dotenv'
-
 dotenv.config()
-
-export const APP_NAME = 'Axios API'
-export const ENVIRONMENT = process.env.NODE_ENV
-
-invariant(ENVIRONMENT, 'Did you set your ENVIRONMENT in your .env file?')
-invariant(APP_NAME, 'Did you set your APP_NAME in your .env file?')
-
-export const HTTP_SERVER_PORT = (env =>
-  ({ production: 3000, test: 4004, development: 5005 }[env] || 6006))(
-  ENVIRONMENT
-)
